aa08764c956d4623d54df02071c7fa59ef13c008
faressuliman <faresashraf20044@gmail.com>
2025-12-01 22:15:14 +0200

    feat: error handling, notfound component, refact: redirect user to home after logging in

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,15 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Shop from "../pages/Shop";
+import Loader from "../components/ui/Loader";
 
 const Home = lazy(() => import("../pages"));
 const About = lazy(() => import("../pages/About"));
 const Contact = lazy(() => import("../pages/Contact"));
 const Login = lazy(() => import("../pages/Login"));
 const Register = lazy(() => import("../pages/Register"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 const ForgotPassword = lazy(() => import("../pages/ForgotPassword"));
 const ResetPassword = lazy(() => import("../pages/ResetPassword"));
 const Wishlist = lazy(() => import("../pages/Wishlist"));
@@ -18,20 +20,105 @@ const Orders = lazy(() => import("../pages/Orders"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
+    <Route path="/" element={<Layout />} errorElement={<NotFound />}>
+      <Route
+        index
+        element={
+          <Suspense fallback={<Loader />}>
+            <Home />
+          </Suspense>
+        }
+      />
       <Route path="shop" element={<Shop />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="forgotpassword" element={<ForgotPassword />} />
-      <Route path="resetpassword" element={<ResetPassword />} />
-      <Route path="checkout" element={<Checkout />} />
-      <Route path="wishlist" element={<Wishlist />} />
-      <Route path="product/:id" element={<Product />} />
-      <Route path="search" element={<SearchResults />} />
-      <Route path="orders" element={<Orders />} />
+      <Route
+        path="about"
+        element={
+          <Suspense fallback={<Loader />}>
+            <About />
+          </Suspense>
+        }
+      />
+      <Route
+        path="contact"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Contact />
+          </Suspense>
+        }
+      />
+      <Route
+        path="login"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Login />
+          </Suspense>
+        }
+      />
+      <Route
+        path="register"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Register />
+          </Suspense>
+        }
+      />
+      <Route
+        path="forgotpassword"
+        element={
+          <Suspense fallback={<Loader />}>
+            <ForgotPassword />
+          </Suspense>
+        }
+      />
+      <Route
+        path="resetpassword"
+        element={
+          <Suspense fallback={<Loader />}>
+            <ResetPassword />
+          </Suspense>
+        }
+      />
+      <Route
+        path="checkout"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Checkout />
+          </Suspense>
+        }
+      />
+      <Route
+        path="wishlist"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Wishlist />
+          </Suspense>
+        }
+      />
+      <Route
+        path="product/:id"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Product />
+          </Suspense>
+        }
+      />
+      <Route
+        path="search"
+        element={
+          <Suspense fallback={<Loader />}>
+            <SearchResults />
+          </Suspense>
+        }
+      />
+      <Route
+        path="orders"
+        element={
+          <Suspense fallback={<Loader />}>
+            <Orders />
+          </Suspense>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
