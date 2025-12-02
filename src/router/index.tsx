3c20cf8a6902cd519edc8871086d3a44ceae026c
Frontman <faresashraf20044@gmail.com>
2025-12-02 04:11:38 +0200

    fix: small changes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,7 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Shop from "../pages/Shop";
-import Loader from "../components/ui/Loader";
 
 const Home = lazy(() => import("../pages"));
 const About = lazy(() => import("../pages/About"));
@@ -21,103 +20,19 @@ const Orders = lazy(() => import("../pages/Orders"));
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />} errorElement={<NotFound />}>
-      <Route
-        index
-        element={
-          <Suspense fallback={<Loader />}>
-            <Home />
-          </Suspense>
-        }
-      />
+      <Route index element={  <Home /> } />
       <Route path="shop" element={<Shop />} />
-      <Route
-        path="about"
-        element={
-          <Suspense fallback={<Loader />}>
-            <About />
-          </Suspense>
-        }
-      />
-      <Route
-        path="contact"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Contact />
-          </Suspense>
-        }
-      />
-      <Route
-        path="login"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Login />
-          </Suspense>
-        }
-      />
-      <Route
-        path="register"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Register />
-          </Suspense>
-        }
-      />
-      <Route
-        path="forgotpassword"
-        element={
-          <Suspense fallback={<Loader />}>
-            <ForgotPassword />
-          </Suspense>
-        }
-      />
-      <Route
-        path="resetpassword"
-        element={
-          <Suspense fallback={<Loader />}>
-            <ResetPassword />
-          </Suspense>
-        }
-      />
-      <Route
-        path="checkout"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Checkout />
-          </Suspense>
-        }
-      />
-      <Route
-        path="wishlist"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Wishlist />
-          </Suspense>
-        }
-      />
-      <Route
-        path="product/:id"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Product />
-          </Suspense>
-        }
-      />
-      <Route
-        path="search"
-        element={
-          <Suspense fallback={<Loader />}>
-            <SearchResults />
-          </Suspense>
-        }
-      />
-      <Route
-        path="orders"
-        element={
-          <Suspense fallback={<Loader />}>
-            <Orders />
-          </Suspense>
-        }
-      />
+      <Route path="about" element={ <About /> } />
+      <Route path="contact" element={ <Contact /> } />
+      <Route path="login" element={ <Login /> } />
+      <Route path="register" element={ <Register /> } />
+      <Route path="forgotpassword" element={<ForgotPassword />} />
+      <Route path="resetpassword" element={ <ResetPassword /> } />
+      <Route path="checkout" element={ <Checkout /> } />
+      <Route path="wishlist" element={ <Wishlist /> } />
+      <Route path="product/:id" element={ <Product />} />
+      <Route path="search" element={<SearchResults />} />
+      <Route path="orders" element={<Orders />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
